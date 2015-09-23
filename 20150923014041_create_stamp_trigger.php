452464cf3f300f63c3e612de281a4a43485d5f86
jdenoc <jdenoc@gmail.com>
2015-09-22 20:51:04 -0500

    Create `entry_creation_timestamp` trigger Trigger sets `create_stamp` timestamp to NOW() on INSERT

---
+++ 20150923014041_create_stamp_trigger.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateStampTrigger extends AbstractMigration {
+
+    /**
+     * Migrate UP.
+     * Create `entry_creation_timestamp` trigger
+     * Trigger sets `create_stamp` timestamp to NOW() on INSERT
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function up(){
+        $this->execute(
+            "CREATE TRIGGER entry_creation_timestamp
+            BEFORE INSERT ON entries
+            FOR EACH ROW
+            SET NEW.create_stamp = NOW();"
+        );
+
+    }
+
+    /**
+     * Migrate DOWN
+     * Drop `entry_creation_timestamp` trigger
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function down(){
+        $this->execute("DROP TRIGGER IF EXISTS entry_creation_timestamp");
+    }
+}
