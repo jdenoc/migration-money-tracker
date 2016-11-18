cf493f17c045d8fe9525a0f04631d21095cbfa34
jdenoc <jdenoc@gmail.com>
2016-11-17 22:50:01 -0600

    Added a migration to add a column to the attachements table column: attachments.uid

--- 20161118040622_add_attachments_column_uid.php
+++ 20161118040622_add_attachments_column_uid.php
@@ -2,18 +2,18 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class AddAttachmentsColumnStamp extends AbstractMigration {
-
+class AddAttachmentsColumnUid extends AbstractMigration
+{
     /**
      * Change Method.
-     * Adds a stamp column to the attachments table
+     * Adds a uid column to the attachments table
      *
      * More information on writing migrations is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
      */
     public function change(){
         $table = $this->table('attachments');
-        $table->addColumn('stamp', 'timestamp', array('default'=>'CURRENT_TIMESTAMP'));
+        $table->addColumn('uid', 'string', array('after'=>'attachment', 'limit'=>50));
         $table->update();
     }
 }
