ed248b40dc2d0f8a4f9193f310e49a95254cb25e
jdenoc <jdenoc@gmail.com>
2016-11-18 01:25:10 -0600

    Replacing the previously new migration, with a newer migration. This new migration will add a unique index to the new uid column of the attachments table

--- 20161118071413_add_unique_attachments_column_uid.php
+++ 20161118071413_add_unique_attachments_column_uid.php
@@ -2,18 +2,20 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class AddAttachmentsColumnUid extends AbstractMigration
-{
+class AddUniqueAttachmentsColumnUid extends AbstractMigration {
+
     /**
      * Change Method.
-     * Adds a uid column to the attachments table
+     * Adds a unique uid column to the attachments table
      *
      * More information on writing migrations is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
      */
     public function change(){
         $table = $this->table('attachments');
-        $table->addColumn('uid', 'string', array('after'=>'attachment', 'limit'=>50));
+        $table->addColumn('uid', 'string', array('after'=>'attachment', 'limit'=>50, 'null' => true));
+        $table->addIndex('uid', array('unique'=>true));
         $table->update();
     }
+
 }
