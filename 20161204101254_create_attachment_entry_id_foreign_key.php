7762d40065e3dc924886eeb9a59cd2b9a6b5fb7b
jdenoc <jdenoc@gmail.com>
2016-12-04 04:31:53 -0600

    Cleaned some PHPDocs up a bit

--- 20161204101254_create_attachment_entry_id_foreign_key.php
+++ 20161204101254_create_attachment_entry_id_foreign_key.php
@@ -6,24 +6,10 @@ class CreateAttachmentEntryIdForeignKey extends AbstractMigration
 {
     /**
      * Change Method.
-     *
-     * Write your reversible migrations using this method.
+     * Creates a foreign key on the attachments.entry_id column linking it to the entries.id column
      *
      * More information on writing migrations is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
      */
     public function change(){
         $this->table('attachments')
